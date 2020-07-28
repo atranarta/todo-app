08cafd2f94b77505932966ae3046a36c34dc4fdf
atranarta <radyuk.tatyana@gmail.com>
2020-07-28 18:00:46 +0200

    Created todo-app project, added basic layout and styles, plugged in fonts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="header">Todo List</h1>
+      <form className="todo-list">
+        <ul>
+          <div className="todo">
+            <div className="checkbox" />
+            <input type="text" value="Todo one" />
+          </div>
+        </ul>
+      </form>
     </div>
   );
 }
